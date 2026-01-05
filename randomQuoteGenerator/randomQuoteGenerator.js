7b4a6197a48a4054e47b811f4f54d99fb6cab8cf
Damisc <dammyoladapo2021@gmail.com>
2026-01-05 03:30:06 +0100

    completed random quote generator UI

--- randomQuoteGenerator/randomQuoteGenerator.js
+++ randomQuoteGenerator/randomQuoteGenerator.js
@@ -1,3 +1,31 @@
+const colors = [
+    ["#0F172A"  ,  "#F8FAFC"],
+    ["#2563EB"  ,  "#E0F2FE"],
+    ["#111827"  ,  "#9CA3AF"],
+    ["#22C55E"  ,  "#ECFDF5"],
+    ["#EF4444 " ,  "#FFF1F2"],
+    ["#7C3AED"  ,  "#EDE9FE"],
+    ["#1F2937"  ,  "#D1D5DB"],
+    ["#F97316"  ,  "#FFF7ED"],
+    ["#14B8A6"  ,  "#CCFBF1"],
+    ["#9333EA"  ,  "#FAE8FF"],
+    ["#020617"  ,  "#F1F5F9"],
+    ["#3B82F6"  ,  "#F9FAFB"],
+    ["#DC2626"  ,  "#FEE2E2"],
+    ["#0EA5E9"  ,  "#ECFEFF"],
+    ["#15803D"  ,  "#BBF7D0"],
+    ["#A855F7"  ,  "#FAFAFA"],
+    ["#334155"  ,  "#CBD5E1"],
+    ["#F59E0B"  ,  "#FEF3C7"],
+    ["#10B981"  ,  "#D1FAE5"],
+    ["#1E293B"  ,  "#E5E7EB"],
+]
+
+function getRandomColorCombo() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex]
+}
+
 function getNewRandomQuote() {
     fetch("https://api.allorigins.win/raw?url=https://zenquotes.io/api/random")
     .then(response => {
@@ -7,6 +35,8 @@ function getNewRandomQuote() {
         return response.json();
     })
     .then(data => {
+        const colorCombo = getRandomColorCombo()
+        document.getElementById("random-quote-generator").style.background = "linear-gradient(45deg, " + colorCombo[0] + " , " + colorCombo[1] + ")"
         const quoteText = data[0].q
         const quoteAuthor = data[0].a
 
