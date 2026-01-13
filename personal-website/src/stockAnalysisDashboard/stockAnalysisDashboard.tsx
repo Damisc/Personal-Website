fabd41b5617b463d0ae174a41195d568f6259a4c
Damisc <dammyoladapo2021@gmail.com>
2026-01-13 03:16:37 +0100

    converted analyzeStock Workflow to react

--- personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
+++ personal-website/src/stockAnalysisDashboard/stockAnalysisDashboard.tsx
@@ -1,32 +1,32 @@
-// import { useState } from 'react'
-
+import { useState } from 'react'
+import { analyzeStock } from "./stockAnalysisDashboard"
 
 function StockAnalysisDashboard() {
 
-    async function runStockAnalysis () {
-        alert("function called!")
-    }
+  const [stockData, setStockData] = useState()
+  const [stockSymbol, setStockSymbol] = useState("")
 
-//   const [count, setCount] = useState(0)
+    async function runStockAnalysis () {
+        const gotStockData = await analyzeStock(stockSymbol)
+        setStockData(gotStockData)
+    }
 
   return (
     <>
-    <div>
-        <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
-        <div id="stock-analysis-dashboard-subtitle">
-            Put in a stock you would like to analyze (e. TSLA)
+        <div>
+            <div id="stock-analysis-dashboard-title">STOCK ANALYSIS DASHBOARD</div>
+            <div id="stock-analysis-dashboard-subtitle">
+                Put in a stock you would like to analyze (e. TSLA)
+            </div>
+            <input
+                value={stockSymbol}
+                onChange = {e => setStockSymbol(e.target.value)}
+            ></input>
+            <button className="stock-analysis-dashboard-button" onClick={() => runStockAnalysis()}>Analyze</button>
+            <div>
+                {JSON.stringify(stockData)}
+            </div>
         </div>
-        <input id="stock-analysis-dashboard-input"></input>
-        <button className="stock-analysis-dashboard-button" onClick={() => runStockAnalysis()}>Analyze</button>
-        <div id="stock-analysis-dashboard-data"></div>
-    </div>
-    
-      {/* <h1>Vite + React in stock Dashboard</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
     </>
   )
 }
