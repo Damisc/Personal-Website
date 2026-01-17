54d2bca4f35fa6e15788a91151ceaebbbf051c17
Damisc <dammyoladapo2021@gmail.com>
2026-01-17 09:06:40 +0100

    added vite custom build step

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+  },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "./src/crapsgame/crapsgame.js",
+          dest: "./src/crapsgame/"
+        },
+        {
+          src: "./src/randomQuoteGenerator/randomQuoteGenerator.js",
+          dest: "./src/randomQuoteGenerator/"
+        },
+        {
+          src: "./src/aboutMeSection/Damilola's CV.pdf",
+          dest: "./src/aboutMeSection/"
+        },
+      ],
+    }),
+  ],
 })
